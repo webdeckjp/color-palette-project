f7054c7d41e9e6fd5c998976dab8209bfe4e0d00
Webdeckjp <63958733+webdeckjp@users.noreply.github.com>
2020-09-22 01:48:21 +0900

    sliders finished

---
+++ app.js
@@ -0,0 +1,162 @@
+//alias
+const cl = function () {
+  return console.log.apply(console, arguments);
+};
+// const gdid = document.getElementById.bind(document);
+// or
+// const gdid = function () {
+//     return document.getElementById.apply(document, arguments);
+// }
+// or
+// const gdid = function(name){return document.getElementById(name)}
+
+//selections
+const colorDivs = document.querySelectorAll(".color");
+const generateBtn = document.querySelector(".generate");
+const sliders = document.querySelectorAll('input[type="range"]');
+const currentHeight = document.querySelectorAll(".color h2");
+let initialColors;
+
+//event listeners
+
+sliders.forEach((slider) => {
+  slider.addEventListener("input", hslControls);
+});
+
+colorDivs.forEach((div, index) => {
+  div.addEventListener("change", () => {
+    updateTextUI(index);
+  });
+});
+
+//functions
+
+// color generator
+// function generateHex() {
+//   const letters = "0123456789ABCDEF";
+//   let hash = "#";
+//   for (let i = 0; i < 6; i -= -1) {
+//     hash += letters[Math.floor(Math.random() * 16)];
+//   }
+//   return hash;
+// }
+
+function generateHex() {
+  const hexColor = chroma.random();
+  return hexColor;
+}
+
+function randomColors() {
+  initialColors = [];
+  colorDivs.forEach((div, index) => {
+    const hexText = div.children[0];
+    const randomColor = generateHex();
+    initialColors.push(chroma(randomColor).hex());
+
+    //add bg color
+    div.style.backgroundColor = randomColor;
+    hexText.innerText = randomColor;
+
+    //check contrast
+    checkTextContrast(randomColor, hexText);
+
+    //initialColorize sliders
+    const color = chroma(randomColor);
+    const sliders = div.querySelectorAll(".sliders input");
+    const hue = sliders[0];
+    const brightness = sliders[1];
+    const saturation = sliders[2];
+
+    colorizeSliders(color, hue, brightness, saturation);
+  });
+
+  resetInputs();
+}
+
+function checkTextContrast(color, text) {
+  const luminance = chroma(color).luminance();
+  if (luminance > 0.5) {
+    text.style.color = "black";
+  } else {
+    text.style.color = "white";
+  }
+}
+
+function colorizeSliders(color, hue, brightness, saturation) {
+  //scale saturation
+  const noSat = color.set("hsl.s", 0);
+  const fullSat = color.set("hsl.s", 1);
+  const scaleSat = chroma.scale([noSat, color, fullSat]);
+  //scale brightness
+  const midBright = color.set("hsl.l", 0.5);
+  const scaleBright = chroma.scale(["black", midBright, "white"]);
+
+  //update Input colors
+  saturation.style.backgroundImage = `linear-gradient(to right,${scaleSat(
+    0
+  )}, ${scaleSat(1)})`;
+
+  brightness.style.backgroundImage = `linear-gradient(to right,${scaleBright(
+    0
+  )},${scaleBright(0.5)}, ${scaleBright(1)})`;
+  hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75), rgb(204,204,75),rgb(75,204,75),rgb(75,204,204),rgb(75,75,204),rgb(204,75,204),rgb(204,75,75))`;
+}
+
+function hslControls(e) {
+  const index =
+    e.target.getAttribute("data-bright") ||
+    e.target.getAttribute("data-sat") ||
+    e.target.getAttribute("data-hue");
+
+  let sliders = e.target.parentElement.querySelectorAll('input[type="range"]');
+  const hue = sliders[0];
+  const brightness = sliders[1];
+  const saturation = sliders[2];
+
+  const bgColor = initialColors[index];
+  cl(bgColor);
+
+  let color = chroma(bgColor)
+    .set("hsl.s", saturation.value)
+    .set("hsl.l", brightness.value)
+    .set("hsl.h", hue.value);
+
+  colorDivs[index].style.backgroundColor = color;
+  //   colorDivs[index].querySelector("h2").innerText = color;
+
+  //colorize sliders/inputs
+  colorizeSliders(color, hue, brightness, saturation);
+}
+
+function updateTextUI(index) {
+  const activeDiv = colorDivs[index];
+  const color = chroma(activeDiv.style.backgroundColor);
+  const textHex = activeDiv.querySelector("h2");
+  const icons = activeDiv.querySelectorAll(".controls button");
+  textHex.innerText = color.hex();
+  checkTextContrast(color, textHex);
+  for (icon of icons) {
+    checkTextContrast(color, icon);
+  }
+}
+
+function resetInputs() {
+  const sliders = document.querySelectorAll(".sliders input");
+  sliders.forEach((slider) => {
+    if (slider.name === "hue") {
+      const hueColor = initialColors[slider.getAttribute("data-hue")];
+      const hueValue = chroma(hueColor).hsl()[0];
+      slider.value = Math.floor(hueValue);
+    } else if (slider.name === "brightness") {
+      const brightColor = initialColors[slider.getAttribute("data-bright")];
+      const brightValue = chroma(brightColor).hsl()[2];
+      slider.value = Math.floor(brightValue * 100) / 100;
+    } else {
+      const satColor = initialColors[slider.getAttribute("data-sat")];
+      const satValue = chroma(satColor).hsl()[1];
+      slider.value = Math.floor(satValue * 100) / 100;
+    }
+  });
+}
+
+randomColors();
